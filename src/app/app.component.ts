e634d8d4cf9a8520507ea68a6ec1469cef5b2fcb
Emma Banuelos <emmabanuelos@icloud.com>
2019-10-30 11:26:32 -0500

    finally got the fetched data to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Satellite } from './satellite';
-import { bindCallback } from 'rxjs';
+// import { bindCallback } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,36 +10,31 @@ import { bindCallback } from 'rxjs';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  // displayList: Satellite[];
   
   
   
   constructor () {
-  //   this.sourceList = [
-  //     new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-  //     new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-  //     new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-  //     new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-  //     new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-  //   ];
-  
+    this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
   
-    window.fetch(satellitesUrl).then(function(response) {
-      response.json().then(function(data) {
+      window.fetch(satellitesUrl).then(function(response) {
+        response.json().then(function(data) {
+        
         let fetchedSatellites = data.satellites;
-
-          let newSatelliteObject = {
-              name: fetchedSatellites[name],
-              type: fetchedSatellites[this.type],
-              orbitType: fetchedSatellites[this.orbitType],
-              operational: fetchedSatellites[this.operational],
-              launchDate: fetchedSatellites[this.launchDate],
-          };
+        
           for (let i =0; i < fetchedSatellites.length; i++){
-            let data.satellites = this.source.List(newSatelliteObject);
-            console.log(i);
+
+            let newSatelliteObject = {
+              name: fetchedSatellites[i].name,
+              type: fetchedSatellites[i].type,
+              orbitType: fetchedSatellites[i].orbitType,
+              operational: fetchedSatellites[i].operational,
+              launchDate: fetchedSatellites[i].launchDate
+          };
+          this.sourceList.push(newSatelliteObject);
           }
-          console.log (newSatelliteObject['name']);
+          // console.log (newSatelliteObject['name']);
 
           
           // TODO: LOOP OVER SATELLITES USE A FOR LOOP TO BUILD YOUR SATELLITE
